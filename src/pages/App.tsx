2f4145d177275ef9e5efd19a3e27d4b3b5984abc
Gabriel <gabrielsalves2k18@gmail.com>
2022-03-16 13:28:02 -0300

    Primeiro commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista';
+import Cronometro from '../components/Cronometro';
+import style from './App.module.scss'
+
+function App() {
+  return (
+    <div className={style.AppStyle}>
+      <Formulario />
+      <Lista />
+      <Cronometro />
+    </div>
+  );
+}
+
+export default App;
